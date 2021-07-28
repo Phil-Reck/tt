92cb182bce970c55fe47bfd927bc45453e84b2b4
Phil-Reck <Philipmatunda@gmail.com>
2021-07-28 04:32:26 +0300

    Project Setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container d-flex">
+        <div class="row">
+            <div class="col-3 p-5">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc2G_a1XOzonf8h9-UTPkFcoaGwOwuXzfUfA&usqp=CAU" class="rounded-circle" alt="">
+            </div>
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Phil-Reck</h1></div>
+            <div class="d-flex pr-5">
+                <div><strong>303</strong>posts</div>
+                <div><strong>29k</strong>followers</div>
+                <div><strong>404</strong>following</div>
+            </div>
+        </div>
+</div>
+@endsection
